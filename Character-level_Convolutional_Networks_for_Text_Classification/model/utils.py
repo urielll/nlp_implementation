a6412d355c4c22844bd734e9ab15298de7c6ca71
aisolab <bsk0130@gmail.com>
2019-03-11 11:41:57 +0900

    Implementing JamoTokenizer

--- Character-level_Convolutional_Networks_for_Text_Classification/model/utils.py
+++ Character-level_Convolutional_Networks_for_Text_Classification/model/utils.py
@@ -1,12 +1,10 @@
-import os, sys
 import re
-import string
-
-# reference
-# https://github.com/neotune/python-korean-handler/blob/master/korean_handler.py
 
 class JamoTokenizer:
+    """JamoTokenizer for korean"""
     def __init__(self):
+        """Instantiating JamoTokenizer
+        """
         # 유니코드 한글 시작 : 44032, 끝 : 55199
         self.__base_code = 44032
         self.__chosung = 588
@@ -20,100 +18,78 @@ class JamoTokenizer:
                                 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ',
                                 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
         # 종성 리스트. 00 ~ 27 + 1(1개 없음)
-        self.__jongsung_list = [' ', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ',
-                                'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ',
-                                'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ',
+        self.__jongsung_list = [' ', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ',
+                                'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ',
                                 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
 
-        self.__token_dic = sorted(list(set(self.__chosung_list + self.__jungsung_list + self.__jongsung_list)))
-        self.__token_dic.extend(['<eng>', '<num>', '<unk>'])
-        self.__token_dic = {token : idx for idx, token in enumerate(self.__token_dic)}
+        self.token2idx = sorted(
+            list(set(self.__chosung_list + self.__jungsung_list + self.__jongsung_list)))
+        self.token2idx = ["<pad>", "<eng>", "<num>", "<unk>"] + self.token2idx
+        self.token2idx = {token: idx for idx, token in enumerate(self.token2idx)}
 
-    def tokenize(self):
-        pass
+    def tokenize(self, string: str) -> list:
+        """Tokenizing string to sequences of indices
 
-    def token2idx(self):
-        pass
+        Args:
+            string: characters
 
+        Returns:
+            sequence of tokens: list of characters
+        """
+        split_string = list(string)
 
-jamo = JamoTokenizer()
-jamo._JamoTokenizer__token_dic
+        sequence_of_tokens = []
+        for char in split_string:
+            # 한글 여부 check 후 분리
+            if re.match(".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*", char) is not None:
+                char_code = ord(char) - self.__base_code
+                alphabet1 = int(char_code / self.__chosung)
+                sequence_of_tokens.append(self.__chosung_list[alphabet1])
+                alphabet2 = int(
+                    (char_code - (self.__chosung * alphabet1)) / self.__jungsung)
+                sequence_of_tokens.append(self.__jungsung_list[alphabet2])
+                alphabet3 = int(
+                    (char_code - (self.__chosung * alphabet1) - (self.__jungsung * alphabet2)))
+                if alphabet3 != 0:
+                    sequence_of_tokens.append(self.__jongsung_list[alphabet3])
+            else:
+                sequence_of_tokens.append(char)
 
-def tokenize_jamo(string:str) -> list:
-    # 유니코드 한글 시작 : 44032, 끝 : 55199
-    base_code, chosung, jungsung = 44032, 588, 28
+        return sequence_of_tokens
 
-    # 초성 리스트. 00 ~ 18
-    chosung_list = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ',
-                    'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ',
-                    'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+    def transform(self, sequence_of_tokens: list) -> list:
+        """Transforming sequences of tokens to sequences of indices
 
-    # 중성 리스트. 00 ~ 20
-    jungsung_list = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ',
-                     'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ',
-                     'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
+        Args:
+            sequence_of_tokens: list of characters
 
-    # 종성 리스트. 00 ~ 27 + 1(1개 없음)
-    jongsung_list = [' ', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ',
-                     'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ',
-                     'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ',
-                     'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+        Returns:
+            sequence_of_indices: list of integers
+        """
+        sequence_of_indices = []
+        for token in sequence_of_tokens:
+            if re.match(".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*", token) is not None:
+                sequence_of_indices.append(self.token2idx.get(token))
+            else:
+                if re.match("[0-9]", token) is not None:
+                    sequence_of_indices.append(self.token2idx.get('<num>'))
+                elif re.match("[A-z]", token) is not None:
+                    sequence_of_indices.append(self.token2idx.get('<eng>'))
+                else:
+                    sequence_of_indices.append(self.token2idx.get('<unk>'))
 
-    split_string = list(string)
+        return sequence_of_indices
 
-    result = []
-    for char in split_string:
-        # 한글 여부 check 후 분리
-        if re.match('.*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*', char) is not None:
-            char_code = ord(char) - base_code
-            alphabet1 = int(char_code / chosung)
-            result.append(chosung_list[alphabet1])
-            alphabet2 = int((char_code - (chosung * alphabet1)) / jungsung)
-            result.append(jungsung_list[alphabet2])
-            alphabet3 = int((char_code - (chosung * alphabet1) - (jungsung * alphabet2)))
-            if alphabet3 != 0:
-                result.append(jongsung_list[alphabet3])
-        else:
-            result.append(char)
+    def tokenize_and_transform(self, string: str) -> list:
+        """Tokenizing and transforming string to sequence
 
-    return result
+        Args:
+            string: characters
 
+        Returns:
+            sequence_of_indices: list of integers
+        """
+        sequence_of_indices = self.transform(self.tokenize(string))
 
-for tok
+        return sequence_of_indices
 
-
-chosung_list = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ',
-                    'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ',
-                    'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
-
-jungsung_list = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ',
-                     'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ',
-                     'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
-
-jongsung_list = [' ', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ',
-                 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ',
-                 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ',
-                  'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
-
-unified_list = []
-
-unified_list.extend(chosung_list)
-unified_list.extend(jungsung_list)
-unified_list.extend(jongsung_list)
-unified_list = set(unified_list)
-unified_list = list(unified_list)
-unified_list= sorted(unified_list)
-unified_list
-
-chosung_list + jungsung_list + jongsung_list
-
-tokenize_jamo('김보서')
-
-str.isalnum('1')
-str.isnumeric('123')
-str.isalpha('ㅑ')
-
-# tokenize 구현시
-import re
-re.match('[A-z]', 'a') is not None
-re.match('[0-9]', 'a') is not None
